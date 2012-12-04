f2b1f8c7a8ef2c9172b6dd3da69173506ddc4ae8
illfang <kevin.kratzer@uei.co.jp>
2012-12-04 20:42:58 +0900

    small changes

--- grunt.js
+++ grunt.js
@@ -22,8 +22,11 @@ module.exports = function(grunt) {
                 mixing: ['dev/header.js', grunt.file.expand('dev/src/*.js').filter(function(file){ return !file.match(/_/); }),'dev/plugins/mixing.enchant.js'],
                 edge: ['dev/header.js', grunt.file.expand('dev/src/*.js').filter(function(file){ return !file.match(/_/); }),'dev/plugins/mixing.enchant.js', 'dev/plugins/edge.enchant.js'] 
             },
-            qunit: {
+            qunit_old: {
                 files: ['tests/qunit/*/*.html']
+            },
+            qunit: {
+                files: ['tests/qunit/edge.enchant.js/*.html']
             },
             concat: {
                 dist: {
@@ -80,6 +83,14 @@ module.exports = function(grunt) {
                         'dev/plugins/*.js',
                     ],
                     tasks: 'lint:core concat min qunit exec:lang'
+                },
+                edge: {
+                    files: [
+                        'dev/src/*.js',
+                        'dev/plugins/edge.enchant.js',
+                        'dev/plugins/mixing.enchant.js'
+                    ],
+                    tasks: 'lint:core lint:mixing lint:edge qunit concat min exec:lang'
                 }
             },
             jshint: {
